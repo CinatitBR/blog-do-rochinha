8ab7a2673a4d2d33700f64da3a6091a7c8299af2
cinatit <cinatit.igor.santos@gmail.com>
2026-03-30 17:52:58 -0300

    Update website global metadata

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Blog do Rochinha";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Olá! Esse é o meu mundinho na internet 👉🌍";
 export const BASE_URL = "/blog-do-rochinha";
